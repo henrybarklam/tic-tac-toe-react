83995c917aaf28aa0f588daa440c59b3d4d29f9d
Henry Barklam <henry@optoinvest.com>
2023-12-26 17:31:13 -0500

    Square component separated out

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-// This code creates a component. 
+// This code creates a component. Must start with a capital letter 
+
+function Square({ value }){
+  return <button className="square">{value}</button>;
+}
 
 
-export default function Square() {
+//We need to pass the value prop from Board to Square so they each have different numbers
+export default function Board() {
   return (
+
+    // Can only return a single element so need to wrap
   <>
   <div className="board-row">
-    <button className="square">1</button>
-    <button className="square">2</button>
-    <button className="square">3</button>
+    <Square value="1" />
+    <Square value="2"/>
+    <Square value="3"/>
+  </div>
+  
+  <div className="board-row">
+    <Square value="4"/>
+    <Square value="5"/>
+    <Square value="6"/>
   </div>
 
   <div className="board-row">
-    <button className="square">4</button>
-    <button className="square">5</button>
-    <button className="square">6</button>
-  </div>
-
-  <div className="board-row">
-    <button className="square">7</button>
-    <button className="square">8</button>
-    <button className="square">9</button>
+    <Square value="7"/>
+    <Square value="8"/>
+    <Square value="9"/>
   </div>
   </>
   );
