f576e1ef991c4a4410159c22c25951e54956d056
Henry Barklam <henry@optoinvest.com>
2023-12-25 22:20:13 -0500

    Two buttons:

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+// This code creates a component. 
+
+
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+  <>
+  <button className="square">X</button>;
+  <button className="square">X</button>;
+  </>
+  );
 }
