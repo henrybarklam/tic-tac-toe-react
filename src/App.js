94d76d0dda720794baa0441eda9f56d02937e863
Henry Barklam <henry@optoinvest.com>
2023-12-26 18:42:42 -0500

    Adding capacity to add the correct square click

--- src/App.js
+++ src/App.js
@@ -2,52 +2,55 @@
 // to let it remember things
 import { useState } from 'react';
 
+//? To collect data from multiple children, or to have two child components communicate with each other
+//? declare the shared state in their parent component instead. The parent component can pass
+//? that state back down to the children via props. This is how you keep child components
+//? in sync with each other and with their parent
 
 
 // This code creates a component. Must start with a capital letter 
-function Square(){
-  //value stores the value and setValue is a function that can be used to change the value
-  // null is the initial state variable
-  const [value, setValue] = useState(null);
-  
-  function handleClick(){
-    console.log('clicked!');
-  }
-
+function Square( {value, onSquareClick} ){
   return (
-    <button 
-      className="square"
-      onClick={handleClick}
-    >
-      {value}
-    </button>
-  );
+    <button className="square" onClick={onSquareClick}>{value}</button>);
 }
 
 
 //We need to pass the value prop from Board to Square so they each have different numbers
 export default function Board() {
+
+  //The set function lets React know that the component has changed
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+
+  // JS supports closures which means that the inner function (handleClick) has access to variables and functions
+  // defined in an outer function
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X"
+    setSquares(nextSquares)
+  }
+
   return (
 
     // Can only return a single element so need to wrap
   <>
-  <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+    <div className="board-row">
+      <Square value={squares[0]} onSquareClick={handleClick}/>
+      <Square value={squares[1]} onSquareClick={handleClick}/>
+      <Square value={squares[2]} onSquareClick={handleClick}/>
     </div>
 
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[3]} onSquareClick={handleClick}/>
+      <Square value={squares[4]} onSquareClick={handleClick}/>
+      <Square value={squares[5]} onSquareClick={handleClick}/>
     </div>
 
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[6]} onSquareClick={handleClick}/>
+      <Square value={squares[7]} onSquareClick={handleClick}/>
+      <Square value={squares[8]} onSquareClick={handleClick}/>
     </div>
   </>
-  );
+    );
 }
