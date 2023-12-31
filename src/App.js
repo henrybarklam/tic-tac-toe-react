eaa5117eaf255e0a5c981a0cedf09b6d1f73e1a9
Henry Barklam <henry@optoinvest.com>
2023-12-31 18:40:49 +0100

    Everything up to status

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default function Board() {
   function handleClick(i){
 
     // You want to return early if the square is already occupied
-    if (squares[i]){
+    if (squares[i] || calculateWinner(squares)){
       return
     }
 
@@ -70,4 +70,26 @@ export default function Board() {
     </div>
   </>
     );
+}
+
+
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+  for (let i = 0; i < lines.length; i++){
+    const [a,b,c] = lines[i];
+    // Why not simplify this?
+    if(squares[a] && squares[a] == squares[b] && squares[a] == squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
 }
